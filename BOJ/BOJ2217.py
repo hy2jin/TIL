bae9ac2cb0e5b677a7372c4bfbeebb35f0086813
이혜진 <hy2jinl22@gmail.com>
2021-08-17 21:49:48 +0900

    BOJ2217 로프

--- BOJ/BOJ2217.py
+++ BOJ/BOJ2217.py
@@ -1,19 +1,11 @@
 # BOJ 2217 로프
 N = int(input())
-rope = []
+rope = [int(input()) for _ in range(N)]
+rope.sort(reverse=True)
+
+maxW = 0
 for _ in range(N):
-    rope.append(int(input()))
-
-def compare():
-    global rope
-    n = len(rope)
-    min_v = min(rope) * n
-    rope.remove(min(rope))
-    next_min = min(rope) * (n-1)
-    return max(min_v, next_min)
-
-while len(rope) > 2:
-    min_v = min(rope) * len(rope)
-    if min_v == compare():
-        break
-
+    if rope[-1] * len(rope) > maxW:
+        maxW = rope[-1] * len(rope)
+    rope.pop()
+print(maxW)
