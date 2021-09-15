37cce93090b4d20e5b9ff08fd310169826f32b3b
이혜진 <hy2jinl22@gmail.com>
2021-09-16 01:24:00 +0900

    fixed swea

--- swea/4466_bestgrade/4466_bestgrade.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.stdin = open('sample_input.txt')
-for tc in range(int(input())):
-    N, K = map(int, input().split())
-    score = list(map(int, input().split()))
-    score.sort(reverse=True)    # 내림차순 정렬하고 앞에서 K개의 합이 최대인 경우다
-    print('#{} {}'.format(tc+1, sum(score[:K])))
