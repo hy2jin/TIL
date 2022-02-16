b6eaaa35623a4990593e1c011eb0a3e99b29591d
이혜진 <hy2jinl22@gmail.com>
2022-02-16 19:50:03 +0000

    BOJ N과 M (8)

---
+++ BOJ/15663_N과M9.py
@@ -0,0 +1,30 @@
+# BOJ N과 M (8)
+import sys
+sys.stdin = open('input.txt')
+
+def recur(idx):
+    if idx == M:
+        print(*lst)
+        return
+    chk = set()
+    for n in range(N):
+        if not u[n] and nums[n] not in chk:
+            chk.add(nums[n])
+            u[n] = 1
+            lst[idx] = nums[n]
+            recur(idx+1)
+            u[n] = 0
+
+N, M = map(int, input().split())
+nums = sorted(map(int, input().split()))
+u = [0] * (N+1)
+lst = [0] * M
+
+chk = set()
+for n in range(N):
+    if nums[n] not in chk:
+        chk.add(nums[n])
+        u[n] = 1
+        lst[0] = nums[n]
+        recur(1)
+        u[n] = 0
