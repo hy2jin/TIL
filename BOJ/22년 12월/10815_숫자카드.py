9ee9a551900d7c9ec0be8eeacc66844299cdaa7d
이혜진 <hy2jinl22@gmail.com>
2022-12-29 17:29:22 +0900

    Create 숫자 카드.py

---
+++ BOJ/22년 12월/10815_숫자카드.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open("input.txt")
+
+N = int(input())
+cards = list(map(int, input().split()))
+cards.sort()
+_ = input()
+nums = list(map(int, input().split()))
+
+def binary_search(n):
+    s, e = 0, N - 1
+    while s <= e:
+        m = (s + e) // 2
+        if cards[m] == n:
+            ans.append(1)
+            return
+        if cards[m] < n:
+            s = m + 1
+        else:
+            e = m - 1
+    ans.append(0)
+    return
+
+ans = []
+for n in nums:
+    binary_search(n)
+print(*ans)
